9ea0b9e5d40adb993a7806e242841a3ce31a7d6f
theshoxh <tairovshohrux@gmail.com>
2022-04-11 17:43:05 +0500

    git init.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+alert("Salom, xush kelibsiz!");
+
+var firstName = prompt("Ismingiz nima!");
+
+var lastName = prompt("Familyangiz nima!");
+
+var age = prompt("Yoshingiz?");
+
+var province = prompt("Viloyat?");
+
+var job = prompt("Qayerda ishlaysiz yoki o'qiysiz?");
+
+var sport = prompt("Qaysi sport turiga qiziqasiz?");
+
+alert("So'rovnomani to'ldirganingiz uchun katta rahmat !!!");
+
+alert("Xayr !!!");
+
+
+console.log(
+  "Mijoz Ismi: " + firstName + "\n" +
+  "Mijoz Familyasi: " + lastName + "\n" +
+  "Mijoz Yoshi: " + age + "\n" +
+  "Mijoz Yashash joyi(viloyat): " + province + "\n" +
+  "Mijoz ish yoki o'qish joyi: " + job + "\n" +
+  "Mijoz yoqtirgan sport turi: " + sport);
